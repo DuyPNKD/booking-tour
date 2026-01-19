26ce6c4b67d771f83208e10113247d4d398cf848
DuyPNKD <165270115+DuyPNKD@users.noreply.github.com>
2026-01-19 20:51:05 +0700

    fix(Momo): Sửa lại cấu hình redirect để trỏ về tên miền vercel thay vì localhost sau khi thanh toán thành công

--- backend/config/momoConfig.js
+++ backend/config/momoConfig.js
@@ -5,8 +5,11 @@ module.exports = {
     secretKey: process.env.MOMO_SECRET_KEY || "K951B6PE1waDMi640xX08PD3vg6EkVlz",
     partnerCode: process.env.MOMO_PARTNER_CODE || "MOMO",
     orderInfo: process.env.MOMO_ORDER_INFO || "Thanh toán đặt tour",
-    redirectUrl: process.env.MOMO_REDIRECT_URL || "http://localhost:3000/payment-result",
-    ipnUrl: process.env.MOMO_IPN_URL || "http://localhost:3000/api/momo/callback", // cần public URL (vd ngrok) khi test thật với MoMo
+
+    // QUAN TRỌNG: Sửa redirectUrl mặc định từ 3000 thành 5173
+    redirectUrl: process.env.MOMO_REDIRECT_URL || "http://localhost:5173/payment-result",
+
+    ipnUrl: process.env.MOMO_IPN_URL || "http://localhost:3000/api/momo/callback",
     requestType: process.env.MOMO_REQUEST_TYPE || "payWithMethod",
     extraData: process.env.MOMO_EXTRA_DATA || "",
     autoCapture: process.env.MOMO_AUTO_CAPTURE ? process.env.MOMO_AUTO_CAPTURE === "true" : true,
